b255acbb9dd395416d8b39f4d205539da090d721
Zandura1212 <108984072+Zandura1212@users.noreply.github.com>
2023-11-19 05:34:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,6 @@ options_volume_lv_list = options_loads['volume_lv']
 
 program_count = len(options_programs_list)
 
-# print("program_count:", i + 1,"/", program_count)
-# print("list:", options_programs_list[i])
-# print("list name:", options_programs_list[i].get('name'))
-# print("list p_name:", options_programs_list[i].get('p_name'))
-# print("list volume_lv:", options_loads['volume_lv'])
-# print("list float(volume_lv):", float(options_loads['volume_lv']))
-
-# 늘리는거
 def volume_p():
 
     for i in range(program_count):
